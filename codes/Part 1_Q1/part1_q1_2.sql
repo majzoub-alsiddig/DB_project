fd7151def36dd5cc8b703ff83dd1557ef0dfba03
looklikelok4 <looklikelo4@gmail.com>
2026-08-03 11:11:25 +0200

    fix q1 2

--- codes/Part 1_Q1/part1_q1_2.sql
+++ codes/Part 1_Q1/part1_q1_2.sql
@@ -1,6 +1,7 @@
--- ============================================================
---  Q1.2 — الـ View: DEPT_SUMMARY
--- ============================================================
+/* 
+  Q1.2  DEPT_SUMMARY
+ */
+
 
 CREATE VIEW IF NOT EXISTS DEPT_SUMMARY (D, C, Total_s, Average_s) AS
 SELECT Dno, COUNT(*), SUM(Salary), AVG(Salary)
